4e55a271785170c46dd998985786beda2af1a176
Jolinwszhang <jolinw@hawaii.edu>
2024-02-01 10:44:01 -1000

    changes to wod

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to path ' + request.path);
-     next();
+    next();
 });
 let root = (typeof argv["rootdir"] != "undefined")?argv["rootdir"] : ".";
 app.use(express.static( root ));
