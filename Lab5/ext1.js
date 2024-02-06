9c4d79dd868de21ca20cfc21d7663744fda12d92
Jolinwszhang <jolinw@hawaii.edu>
2024-02-06 12:23:13 -1000

    fixed to make pretty

---
+++ Lab5/ext1.js
@@ -0,0 +1,8 @@
+age =  21;
+fav_num = 247;
+day_of_birth = 16;
+month_of_birth = 7;
+console.log(
+    age + fav_num / day_of_birth * month_of_birth,
+    (age + fav_num) / day_of_birth * month_of_birth
+    );
