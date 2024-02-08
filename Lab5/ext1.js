03258afa6d9535d37c7cc89cf4ff6f783552cd7d
Jolinwszhang <jolinw@hawaii.edu>
2024-02-08 11:11:28 -1000

    Lab 5 added

--- Lab5/ext1.js
+++ Lab5/ext1.js
@@ -1,8 +1,18 @@
-age =  21;
-fav_num = 247;
-day_of_birth = 16;
-month_of_birth = 7;
+// Declare and initaize variable 
+age =  20;
+fav_num = 2;
+day_of_birth = 29;
+month_of_birth = 11;
+
+// Expressions
 console.log(
     age + fav_num / day_of_birth * month_of_birth,
     (age + fav_num) / day_of_birth * month_of_birth
     );
+
+//Output
+document.getElementById("result1").innerHTML = 
+calculations1;
+
+document.getElementById("result2").innerHTML = 
+calculations2;
